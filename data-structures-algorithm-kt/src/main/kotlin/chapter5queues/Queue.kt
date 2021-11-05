61d31b82fdca9ba2399054445cb3f8816760c592
chicorasia <chicorasia@gmail.com>
2021-11-05 08:43:55 -0300

    Added Queue interface

---
+++ data-structures-algorithm-kt/src/main/kotlin/chapter5queues/Queue.kt
@@ -0,0 +1,16 @@
+package chapter5queues
+
+interface Queue<T> {
+
+    val count: Int
+
+    val isEmpty: Boolean
+        get() = count == 0
+
+    fun enqueue(element: T) : Boolean
+
+    fun dequeue() : T?
+
+    fun peek() : T?
+
+}
